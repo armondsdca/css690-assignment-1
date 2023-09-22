8ce74b3e00b62ad34f463c526ed80ccc1c24177e
armondsdca <armondaria@yahoo.com>
2023-09-21 23:58:41 -0700

    Step 5

---
+++ js/script.js
@@ -0,0 +1,58 @@
+/*   ***********************
+              STEP 1 
+     ***********************         
+    let some_month 
+    let someMonth
+
+    function the Month()
+    function theMonth()
+
+    let current-month
+    let currentMonth
+
+    let summer_month
+    let summerMonth
+
+    let MyLibrary-function
+    let MyLibraryFunction
+Step */
+
+
+/*   ***********************
+              STEP 2 
+     ***********************  
+
+    3.9 				//numeric literal 
+    'Hello!' 			// string literal 
+    false 				// Boolean literal 
+    null 				// literal null value 
+Step */
+
+/*   ***********************
+              STEP 3 
+     ***********************  
+
+    let anExpression = 3 * (4 / 5) + 6
+    Let step3Expression = 9 * 2 
+Step */
+
+/*   ***********************
+              STEP 4 
+     ***********************  
+
+    let firstName,  LastName, strAddress, strCity, strState, zipCode, yourAge, referralSource, mayWeContactYou
+
+Step */
+
+/*   ***********************
+              STEP 5 
+     ***********************  
+
+    const firstName = 'Armond' , zipCode = 92122, mayWeContactYou = false
+
+Step */
+
+
+
+
+
